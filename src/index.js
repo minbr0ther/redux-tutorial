49d9e0f3ace3159080b25fa9d91b0ae98b5ca179
minbr0ther <minbr0ther@naver.com>
2022-01-07 15:25:56 +0900

    #1.0 Vanilla Counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const add = document.querySelector('#add');
+const minus = document.querySelector('#minus');
+const number = document.querySelector('span');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let count = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const updateText = () => {
+  number.innerText = count;
+};
+
+add.addEventListener('click', () => {
+  count = count + 1;
+  updateText();
+});
+
+minus.addEventListener('click', () => {
+  count = count - 1;
+  updateText();
+});

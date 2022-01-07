8e53a415d6a2c002c7f5f820683cd2d190cc6b7c
minbr0ther <minbr0ther@naver.com>
2022-01-07 16:04:04 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -9,9 +9,21 @@ const add = document.querySelector('#add');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
 
-const countModifier = (state = 0) => {
-  console.log(state);
-  return state;
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  } else {
+    return 0;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+
+console.log(countStore.getState());

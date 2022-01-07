af076b0f4971d80bc42d958fea15a40a4b99707d
minbr0ther <minbr0ther@naver.com>
2022-01-07 15:41:47 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
+import { createStore } from 'redux';
+/*
+✅ Store는 data를 저장하는 곳
+✅ CreateStore는 reducer를 요구함.
+✅ Reducer는 data를 modify 해주는 함수로 reducer가 return하는 것은 application에 있는 data가 됨.
+*/
+
 const add = document.querySelector('#add');
 const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
 
-let count = 0;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (state = 0) => {
+  console.log(state);
+  return state;
 };
 
-add.addEventListener('click', () => {
-  count = count + 1;
-  updateText();
-});
-
-minus.addEventListener('click', () => {
-  count = count - 1;
-  updateText();
-});
+const countStore = createStore(countModifier);

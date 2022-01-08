db36e925c7b218d6ceedef6fd2f8e3e24efaee67
minbr0ther <minbr0ther@naver.com>
2022-01-08 15:37:08 +0900

    #1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -10,20 +10,31 @@ const minus = document.querySelector('#minus');
 const number = document.querySelector('span');
 
 const countModifier = (count = 0, action) => {
-  if (action.type === 'ADD') {
-    return count + 1;
-  } else if (action.type === 'MINUS') {
-    return count - 1;
-  } else {
-    return 0;
+  switch (action.type) {
+    case 'ADD':
+      return count + 1;
+    case 'MINUS':
+      return count - 1;
+    default:
+      return 0;
   }
 };
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: 'ADD' });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: 'MINUS' });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
